c1b2348eab0c1a341e42b984af38859fdab12685
Gianluca Benassi <benox1199@gmail.com>
2022-02-02 15:48:32 +0100

    Current series

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,15 +10,35 @@
         <img src="{{asset('/img/jumbotron.jpg')}}" alt="jumbo image">
     </div>
 
-    <div class="container">
-
-        <div class="series">
+    <div class="series">
+        <div class="container">
             
-        </div>
+            {{-- title --}}
+            <h2>Current series</h2>
 
-        <div class="shop">
-            
-        </div>
+            {{-- comics row --}}
+            <div class="row">
 
+                @foreach ($comics as $comic)
+                    <a href="#" class="card">
+                        <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+                        <span>{{$comic["title"]}}</span>
+                    </a>
+                @endforeach
+
+            </div>
+
+            {{-- load more button --}}
+            <div class="row">
+                <a href="#" class="load-more">load more</a>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="shop">
+        <div class="container">
+
+        </div>
     </div>
 @endsection
