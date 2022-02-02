6279bb8c8aec805b00cd3d55dcb494ce1b86c588
Gianluca Benassi <benox1199@gmail.com>
2022-02-02 14:43:22 +0100

    header-top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    header content
+
+    <div class="header-top">
+        <div class="container">
+            <a href="#">dc power visa</a>
+            <a href="#">additional dc sites &#9660;</a>
+        </div>
+    </div>
+    
+    <div class="header-bottom">
+        
+    </div>
+        
 </header>
