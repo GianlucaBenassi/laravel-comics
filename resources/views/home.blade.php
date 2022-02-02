64bdbc2d5819307f574209af8b76e8e42448382c
Gianluca Benassi <benox1199@gmail.com>
2022-02-02 15:16:14 +0100

    jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,20 @@
 @endsection
 
 @section('pageContent')
-    main content
+
+    <div class="jumbo">
+        <img src="{{asset('/img/jumbotron.jpg')}}" alt="jumbo image">
+    </div>
+
+    <div class="container">
+
+        <div class="series">
+            
+        </div>
+
+        <div class="shop">
+            
+        </div>
+
+    </div>
 @endsection
