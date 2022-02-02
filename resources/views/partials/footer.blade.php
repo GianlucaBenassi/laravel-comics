fb9aac0ed5d6523580ea962611291a2ea1b91bce
Gianluca Benassi <benox1199@gmail.com>
2022-02-02 16:38:03 +0100

    footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,75 @@
 <footer>
-    footer content
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                {{-- left column with links --}}
+                <div class="left">
+
+                    <div class="col">
+                        <h4>dc comics</h4>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movie</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+
+                        <h4>shop</h4>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col">
+                        <h4>dc</h4>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Provacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col">
+                        <h4>sites</h4>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                {{-- right column --}}
+                <div class="right">
+                    <!-- only background image -->
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+            
+        </div>
+    </div>
+
 </footer>
